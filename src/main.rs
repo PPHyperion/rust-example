19d08ff2c819f0cf6412ccb131ac2f622f819d7f
PP_Hyperion <paul.pulter@gmail.com>
2023-04-29 19:18:31 +0200

    add actix_web Hello World!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpRequest, HttpServer, Result};
+
+#[get("/{name}")]
+async fn hello(req: HttpRequest) -> Result<String> {
+    let name = req.match_info().get("name").unwrap_or("World");
+    Ok(format!("Hello {}!", &name))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(hello))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
