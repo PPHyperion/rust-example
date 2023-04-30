27c22472579a07cc617c4e720d86e052bca230e4
PP_Hyperion <paul.pulter@gmail.com>
2023-04-30 14:03:11 +0200

    use dynamic port for tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::net::TcpListener;
+
 use actix_web::dev::Server;
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 
@@ -6,9 +8,9 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| App::new().service(health_check))
-        .bind(("127.0.0.1", 8080))?
+        .listen(listener)?
         .run();
 
     Ok(server)
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::net::TcpListener;
+
 use zero2prod::run;
 
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:8080").expect("Failed to bind port 8080");
+    run(listener)?.await
 }
