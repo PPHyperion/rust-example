af5dec6aebde224f8dbfeed9b81a756652b94706
PP_Hyperion <paul.pulter@gmail.com>
2023-04-30 14:55:31 +0200

    extract spawn_app into module

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,8 +1,8 @@
-use std::net::TcpListener;
+mod common;
 
 #[tokio::test]
 async fn health_check_works() {
-    let address = spawn_app();
+    let address = common::spawn_app();
     let client = reqwest::Client::new();
 
     let response = client
@@ -13,13 +13,4 @@ async fn health_check_works() {
 
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
-}
-
-fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind address");
-    let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
-
-    let _ = tokio::spawn(server);
-    format!("http://127.0.0.1:{}", port)
 }
