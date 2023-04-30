42d300b2512ba1a1f9e8e07ee4683ad0fdf17d2c
PP_Hyperion <paul.pulter@gmail.com>
2023-04-30 21:56:36 +0200

    add ability to skip docker command

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -17,13 +17,16 @@ DB_NAME="${POSTGRES_DB:=newsletter}"
 DB_PORT="${POSTGRES_PORT:=5432}"
 DB_HOST="${POSTGRES_HOST:=localhost}"
 
-docker run \
-	-e POSTGRES_USER=${DB_USER} \
-	-e POSTGRES_PASSWORD=${DB_PASSWORD} \
-	-e POSTGRES_DB=${DB_NAME} \
-	-p "${DB_PORT}":5432 \
-	-d postgres:alpine \
-	postgres -N 1000
+if [[ -z "${SKIP_DOCKER}" ]]
+then
+	docker run \
+		-e POSTGRES_USER=${DB_USER} \
+		-e POSTGRES_PASSWORD=${DB_PASSWORD} \
+		-e POSTGRES_DB=${DB_NAME} \
+		-p "${DB_PORT}":5432 \
+		-d postgres:alpine \
+		postgres -N 1000
+fi
 
 DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
 export DATABASE_URL
