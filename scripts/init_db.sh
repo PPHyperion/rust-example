70bce38391611e4817041d5a182459f35fdc7052
PP_Hyperion <paul.pulter@gmail.com>
2023-04-30 20:14:15 +0200

    check for sqlx and create db

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,6 +2,15 @@
 set -x
 set -eo pipefail
 
+if ! [ -x "$(command -v sqlx)" ]; then
+	echo >&2 "Error: sqlx is not installed."
+	echo >&2 "Use: "
+	echo >&2 "  cargo install sqlx-cli \
+    --no-default-features --features rustls,postgres"
+	echo >&2 "to install it."
+	exit 1
+fi
+
 DB_USER=${POSTGRES_USER:=postgres}
 DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 DB_NAME="${POSTGRES_DB:=newsletter}"
@@ -9,9 +18,13 @@ DB_PORT="${POSTGRES_PORT:=5432}"
 DB_HOST="${POSTGRES_HOST:=localhost}"
 
 docker run \
-  -e POSTGRES_USER=${DB_USER} \
-  -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-  -e POSTGRES_DB=${DB_NAME} \
-  -p "${DB_PORT}":5432 \
-  -d postgres:alpine \
-  postgres -N 1000
+	-e POSTGRES_USER=${DB_USER} \
+	-e POSTGRES_PASSWORD=${DB_PASSWORD} \
+	-e POSTGRES_DB=${DB_NAME} \
+	-p "${DB_PORT}":5432 \
+	-d postgres:alpine \
+	postgres -N 1000
+
+DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
+export DATABASE_URL
+sqlx database create
