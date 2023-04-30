560cc5403c31f00c9850bd0528d372548da42bf4
PP_Hyperion <paul.pulter@gmail.com>
2023-04-30 22:23:16 +0200

    refactor structure

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,10 +1,3 @@
-use std::net::TcpListener;
+mod startup;
 
-pub fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind address");
-    let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
-
-    let _ = tokio::spawn(server);
-    format!("http://127.0.0.1:{}", port)
-}
+pub use startup::*;
