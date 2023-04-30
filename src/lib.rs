f4d18034ef232292d053a6e93988e61fd9fc5632
PP_Hyperion <paul.pulter@gmail.com>
2023-04-30 17:11:53 +0200

    add serde for handling FormData

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,26 @@
 use std::net::TcpListener;
 
 use actix_web::dev::Server;
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/health_check")]
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+#[post("/subscriptions")]
+async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
+    HttpResponse::Ok()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().service(health_check))
+    let server = HttpServer::new(|| App::new().service(health_check).service(subscribe))
         .listen(listener)?
         .run();
 
