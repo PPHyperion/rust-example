d900eec2fead1249f2b65006a8d1451e6ca6db87
PP_Hyperion <paul.pulter@gmail.com>
2023-05-14 13:00:19 +0200

    add env_logger crate for logging to terminal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use env_logger::Env;
 use std::net::TcpListener;
 
 use sqlx::PgPool;
@@ -5,6 +6,8 @@ use zero2prod::{configuration::get_configuration, startup::run};
 
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let configuration = get_configuration().expect("Failed to read configuration");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
