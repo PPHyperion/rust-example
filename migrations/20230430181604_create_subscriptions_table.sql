14154207a3c86801c78d110f529d88a87b3acc39
PP_Hyperion <paul.pulter@gmail.com>
2023-06-17 18:13:23 +0200

    format sql script

--- migrations/20230430181604_create_subscriptions_table.sql
+++ migrations/20230430181604_create_subscriptions_table.sql
@@ -1,8 +1,7 @@
-CREATE TABLE
-  subscriptions (
-    id uuid not null,
-    primary key (id),
-    email text not null unique,
-    name text not null,
-    subscribed_at timestamptz not null
-  )
+CREATE TABLE subscriptions (
+  id uuid not null,
+  primary key (id),
+  email text not null unique,
+  name text not null,
+  subscribed_at timestamptz not null
+)
